139b969279165521e3ae8ce531bf68863a51895e
kyzym <kyzym1@gmail.com>
2023-11-05 10:51:05 +0100

    refactor: update openapi version

--- controllers/swagger.js
+++ controllers/swagger.js
@@ -2,12 +2,18 @@ import { usersSwagger } from './users/index.js';
 import { ordersSwagger } from './orders/index.js';
 
 export const swaggerControllers = {
-  openapi: '3.0.0',
+  openapi: '3.1.0',
   info: {
     title: 'API for IDLO backend',
     version: '1.0.0',
     description: 'Documentation for IDLO controllers',
   },
+  servers: [
+    {
+      url: 'http://localhost:PORT',
+      description: 'Development server',
+    },
+  ],
   paths: {
     ...usersSwagger.paths,
     ...ordersSwagger.paths,
