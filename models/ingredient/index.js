763a055aa50d6ff6fd271034c9eb36f7a72a07e8
Nikolay Naumenko <nnv2006@ukr.net>
2023-11-17 12:46:19 +0200

    UPD: re-export

--- models/ingredient/index.js
+++ models/ingredient/index.js
@@ -1,2 +1,2 @@
-export * from './Ingredient.model.js';
-export * from './ingredient.validation.js';
+export { Ingredient } from './Ingredient.model.js';
+export { addIngredientSchema } from './ingredient.validation.js';
