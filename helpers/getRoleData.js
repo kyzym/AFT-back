c6c9f4022b43c06f4bf10e9a9f9065ff22f84d05
NikolayVerstak <verstak.n.a@gmail.com>
2023-11-18 14:43:57 +0200

    refactor: Use role constants in getRoleData func instead of hardcoding role strings

--- helpers/getRoleData.js
+++ helpers/getRoleData.js
@@ -2,16 +2,19 @@ import User from '../models/user/index.js';
 import Chef from '../models/chef/index.js';
 import Courier from '../models/courier/index.js';
 import Admin from '../models/admin/index.js';
+import { roles } from '../constants/roles.js';
 
 export const getRoleData = async (role, id) => {
   switch (role) {
-    case 'chef':
+    case roles.USER:
+      return User.findById(id);
+    case roles.CHEF:
       return Chef.findById(id);
-    case 'courier':
+    case roles.COURIER:
       return Courier.findById(id);
-    case 'admin':
+    case roles.ADMIN:
       return Admin.findById(id);
     default:
-      return User.findById(id);
+      return null;
   }
 };
