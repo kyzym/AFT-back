0782dd57d94b4a45cad829db8c9e3f8165c29f09
Nikolay Naumenko <nnv2006@ukr.net>
2023-11-21 16:09:30 +0200

    UPD: index.js for reviews

---
+++ controllers/reviews/getReviewById(possible we dont need it!!!)/swagger.js
@@ -0,0 +1,90 @@
+// export const getReviewById = {
+//   components: {
+//     schemas: {
+//       Review: {
+//         type: 'object',
+//         properties: {
+//           id: {
+//             type: 'string',
+//           },
+//           owner: {
+//             type: 'string',
+//           },
+//           dish: {
+//             type: 'string',
+//           },
+//           rating: {
+//             type: 'integer',
+//           },
+//           review: {
+//             type: 'string',
+//           },
+//         },
+//       },
+//     },
+//   },
+//   tags: [
+//     {
+//       name: 'Reviews',
+//       description: 'The reviews managing API',
+//     },
+//   ],
+//   paths: {
+//     '/reviews/{reviewId}': {
+//       get: {
+//         summary: 'Get a review by ID',
+//         tags: ['Reviews'],
+//         parameters: [
+//           {
+//             in: 'path',
+//             name: 'reviewId',
+//             schema: {
+//               type: 'string',
+//             },
+//             required: true,
+//             description: 'ID of the review to retrieve',
+//           },
+//           {
+//             in: 'header',
+//             name: 'Authorization',
+//             required: true,
+//             schema: {
+//               type: 'string',
+//             },
+//             description: 'Bearer token for authentication',
+//           },
+//         ],
+//         responses: {
+//           200: {
+//             description: 'The review',
+//             content: {
+//               'application/json': {
+//                 schema: {
+//                   $ref: '#/components/schemas/Review',
+//                 },
+//               },
+//             },
+//           },
+//           401: {
+//             description: 'Unauthorized - Missing or invalid token',
+//           },
+//           403: {
+//             description:
+//               "Forbidden - User doesn't have permission to access this review",
+//           },
+//           404: {
+//             description: 'Review not found',
+//           },
+//           500: {
+//             description: 'Internal Server Error',
+//           },
+//         },
+//         security: [
+//           {
+//             BearerAuth: [],
+//           },
+//         ],
+//       },
+//     },
+//   },
+// };
