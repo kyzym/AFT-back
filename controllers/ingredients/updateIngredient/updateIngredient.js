de6a21a0da1ff8349e475eb48ca2aa7f69382433
Nikolay Naumenko <nnv2006@ukr.net>
2023-11-21 10:10:39 +0200

    ADD: updateIngredient

---
+++ controllers/ingredients/updateIngredient/updateIngredient.js
@@ -0,0 +1,33 @@
+import { NotFoundError } from '../../../helpers/errors.js';
+import { ctrlWrapper } from '../../../middlewares/ctrlWrapper.js';
+import { validate } from '../../../middlewares/validation.middleware.js';
+import { Ingredient } from '../../../models/ingredient/Ingredient.model.js';
+import { addIngredientSchema } from '../../../models/ingredient/ingredient.validation.js';
+
+export const updateIngredientController = async (req, res) => {
+  const { ingredientId } = req.params;
+  const result = await Ingredient.findByIdAndUpdate(ingredientId, req.body, {
+    new: true,
+  });
+
+  if (!result) {
+    throw NotFoundError(404, 'Not found');
+  }
+  res.status(200).json(result);
+};
+
+export const updateIngredient = (router) => {
+  router.put(
+    '/:ingredientId',
+    // add authenticate middleware
+    // authenticate,
+    // isValidId('ingredientId'),
+    validate(addIngredientSchema),
+    ctrlWrapper(updateIngredientController)
+  );
+};
+
+// We need to check!!!!
+//   authenticate,
+//   isValidId,
+//   validateBody(schemas.addSchema),
