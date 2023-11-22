dc1d19d00e1815e03c68c535cf007bba7b2bdcf7
Nikolay Naumenko <nnv2006@ukr.net>
2023-11-22 09:57:32 +0200

    UPD: addIngredient

--- controllers/ingredients/addIngredient/addIngredient.js
+++ controllers/ingredients/addIngredient/addIngredient.js
@@ -1,10 +1,6 @@
 import { Ingredient } from '../../../models/ingredient/Ingredient.model.js';
-import { NotFoundError } from '../../../helpers/errors.js';
 
 export const addIngredient = async (req, res) => {
-  const ingredient = await Ingredient.create(req.body);
-  if (!ingredient) {
-    throw new NotFoundError('Not found');
-  }
+  await Ingredient.create(req.body);
   res.status(201).json({ message: 'Ingredient created successfully' });
 };
