bffef333f6af6d7f3ebd8909e06eb2b0c1e7d1db
kyzym <kyzym1@gmail.com>
2023-12-07 14:49:45 +0100

    chore: remove log

--- controllers/chefs/getOwnChefOrders/index.js
+++ controllers/chefs/getOwnChefOrders/index.js
@@ -3,7 +3,6 @@ import Order from '../../../models/order/Order.model.js';
 
 export const getOwnChefOrders = async (req, res) => {
   const chefId = req.roleIds.chef;
-  console.log(chefId);
 
   const chefOrders = await Order.find({
     chefId,
