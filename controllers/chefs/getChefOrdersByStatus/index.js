cdb23f8c9ed1bf98829218dc77f8c26be39eb8be
Andrii Zaimak <dev.andrii.zaimak@gmail.com>
2023-12-08 21:39:40 +0200

    refactor: change status to statusCode in every route

--- controllers/chefs/getChefOrdersByStatus/index.js
+++ controllers/chefs/getChefOrdersByStatus/index.js
@@ -1,10 +1,15 @@
-import { NotFoundError } from '../../../helpers/index.js';
+import { NotFoundError, getOrderCodeByValue } from '../../../helpers/index.js';
 import Order from '../../../models/order/Order.model.js';
 
 export const getChefOrdersByStatus = async (req, res) => {
   const { chefId, status } = req.params;
 
-  const chefOrdersByStatus = await Order.find({ chefId, status });
+  // TODO: Change status code
+  // const chefOrdersByStatus = await Order.find({ chefId, status });
+  const chefOrdersByStatus = await Order.find({
+    chefId,
+    statusCode: getOrderCodeByValue(status),
+  });
 
   if (!chefOrdersByStatus) {
     throw new NotFoundError(`Orders for chef with status ${status} not found`);
--- controllers/chefs/updateChefOrderStatus/index.js
+++ controllers/chefs/updateChefOrderStatus/index.js
@@ -1,23 +1,39 @@
-import { orderStatus } from '#constants/orderStatus.js';
-import { ForbiddenError, NotFoundError } from '../../../helpers/index.js';
+import { orderStatuses } from '#constants/orderStatus.js';
+import {
+  ForbiddenError,
+  NotFoundError,
+  getOrderCodeByValue,
+} from '../../../helpers/index.js';
 import Order from '../../../models/order/Order.model.js';
 
 export const updateChefOrderStatus = async (req, res) => {
   const { orderId } = req.params;
   const { status: updateStatus } = req.body;
 
-  if (
-    !Object.prototype.hasOwnProperty.call(
-      orderStatus,
-      updateStatus.toUpperCase()
-    )
-  ) {
+  // TODO: Change status code
+  // if (
+  //   !Object.prototype.hasOwnProperty.call(
+  //     orderStatus,
+  //     updateStatus.toUpperCase()
+  //   )
+  // ) {
+  //   throw new ForbiddenError('Invalid order status');
+  // }
+  if (!orderStatuses.includes(updateStatus)) {
     throw new ForbiddenError('Invalid order status');
   }
 
+  // TODO: Change status code
+  // const newChefOrderStatus = await Order.findByIdAndUpdate(
+  //   orderId,
+  //   { status: updateStatus },
+  //   {
+  //     new: true,
+  //   }
+  // );
   const newChefOrderStatus = await Order.findByIdAndUpdate(
     orderId,
-    { status: updateStatus },
+    { statusCode: getOrderCodeByValue(updateStatus) },
     {
       new: true,
     }
