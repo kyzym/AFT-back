8fcbc3b41e922ce2a4e0df4043ff250a9169c9f8
vladochka2812 <vladochkamoroz2812@gmail.com>
2023-12-09 15:20:03 +0200

    bugfix: fixed serch for chefs

--- controllers/chefs/getChefs/index.js
+++ controllers/chefs/getChefs/index.js
@@ -9,17 +9,18 @@ export const getChefs = async (req, res) => {
     query.isAvailable = req.query.isAvailable === workStatus.ACTIVE;
   }
   const chefs = await query.exec();
-  const chefsList = [];
+  let chefsList = [];
   if (req.query.name) {
     const regex = new RegExp(req.query.name, 'i');
-
     for (const chef of chefs) {
       if ((chef.userId.firstName + chef.userId.lastName).match(regex)) {
         chefsList.push(chef);
       }
     }
+  } else {
+    chefsList = [...chefs];
   }
-  const total = await Chef.countDocuments(query.getFilter());
+
   const promises = chefs.map((chef) => getRating(chef.id));
   const ratings = await Promise.all(promises);
 
@@ -32,6 +33,7 @@ export const getChefs = async (req, res) => {
   const limit = parseInt(req.query.limit) || 10;
   const skipIndex = (page - 1) * limit;
   query = query.skip(skipIndex).limit(limit);
+  const total = await Chef.countDocuments(query.getFilter());
 
   res.status(200).json({
     mappedChefs,
