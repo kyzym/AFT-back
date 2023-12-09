9cf643d7f62843da15364dae82d7cafee6c8acbe
kyzym <kyzym1@gmail.com>
2023-12-09 12:24:54 +0100

    bugfix: id transform in dish controllers

--- controllers/dishes/getPopularDishes/index.js
+++ controllers/dishes/getPopularDishes/index.js
@@ -4,7 +4,13 @@ export const getPopularDishes = async (_req, res) => {
   //It's a mock logic
   const sampleSize = 2;
 
-  const dishes = await Dish.aggregate([{ $sample: { size: sampleSize } }]);
+  // const dishes = await Dish.aggregate([{ $sample: { size: sampleSize } }]);
 
-  res.status(200).json(dishes);
+  // res.status(200).json(dishes);
+
+  const dishes = await Dish.aggregate([{ $sample: { size: sampleSize } }]);
+  const hydratedDishes = dishes.map((dish) => Dish.hydrate(dish));
+  const transformedDishes = hydratedDishes.map((dish) => dish.toJSON());
+
+  res.status(200).json(transformedDishes);
 };
