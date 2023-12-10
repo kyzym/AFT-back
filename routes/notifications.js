70875b232344ebfd27f50b013ff4fc8038ff791e
kyzym <kyzym1@gmail.com>
2023-12-10 09:53:54 +0100

    feat/create-model

---
+++ routes/notifications.js
@@ -0,0 +1,10 @@
+import express from 'express';
+// import { ctrlWrapper } from '../middlewares/ctrlWrapper.js';
+// import { verifyToken } from '#middlewares/auth.middleware.js';
+// import { roles } from '#constants/roles.js';
+
+const router = express.Router();
+
+router.get();
+
+export default router;
