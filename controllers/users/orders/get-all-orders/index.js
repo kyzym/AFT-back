d152414e2ceb70a0702f5f1c17c8597d5259c5bb
Andrii Zaimak <dev.andrii.zaimak@gmail.com>
2023-12-14 09:34:01 +0200

    feat: add pagination

--- controllers/users/orders/get-all-orders/index.js
+++ controllers/users/orders/get-all-orders/index.js
@@ -1,3 +1,4 @@
+import { withPagination } from '#helpers/withPagination.js';
 import { ctrlWrapper } from '#middlewares/ctrlWrapper.js';
 import Order from '#models/order/Order.model.js';
 import { selectOrderData } from '../helpers.js';
@@ -5,27 +6,32 @@ import { selectOrderData } from '../helpers.js';
 const controller = async (req, res) => {
   const { userId } = req.params;
 
-  const data = await Order.find({ userId })
-    .populate({
-      path: 'chef',
-      select: { id: true, user: true, userId: true },
-      populate: {
-        path: 'user',
-        select: { firstName: true, lastName: true },
-      },
-    })
-    .populate('items.dish', 'name image price')
-    .select({
-      __v: false,
-      items: {
-        createdAt: false,
-        updatedAt: false,
-      },
-    })
-    .sort('statusCode -createdAt')
-    .exec();
+  const [orders, pagination] = await withPagination(
+    Order.find({ userId })
+      .populate({
+        path: 'chef',
+        select: { id: true, user: true, userId: true },
+        populate: {
+          path: 'user',
+          select: { firstName: true, lastName: true },
+        },
+      })
+      .populate('items.dish', 'name image price')
+      .select({
+        __v: false,
+        items: {
+          createdAt: false,
+          updatedAt: false,
+        },
+      })
+      .sort('statusCode -createdAt'),
+    req.query
+  );
 
-  return res.json({ success: true, data: { orders: selectOrderData(data) } });
+  return res.json({
+    success: true,
+    data: { orders: selectOrderData(orders), ...pagination },
+  });
 };
 
 export const getAllUserOrders = ctrlWrapper(controller);
