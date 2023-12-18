dcbfe99a37f80b2c2cba60c914365f8388ad1250
kyzym <kyzym1@gmail.com>
2023-12-18 13:22:41 +0100

    feat: courier notifications

--- controllers/couriers/updateCourierOrderStatus/index.js
+++ controllers/couriers/updateCourierOrderStatus/index.js
@@ -1,10 +1,11 @@
 import { orderStatus, orderStatuses } from '#constants/orderStatus.js';
+import { createUserOrderStatusNotification } from '#controllers/notifications/index.js';
 import {
   ForbiddenError,
   NotFoundError,
   getOrderCodeByValue,
-} from '../../../helpers/index.js';
-import Order from '../../../models/order/Order.model.js';
+} from '#helpers/index.js';
+import Order from '#models/order/Order.model.js';
 
 export const updateCourierOrderStatus = async (req, res) => {
   const courierId = req.roleIds.courier;
@@ -34,6 +35,15 @@ export const updateCourierOrderStatus = async (req, res) => {
       orderId,
       { statusCode: getOrderCodeByValue(updateStatus) },
       { new: true }
+    );
+  }
+
+  if (newCourierOrderStatus) {
+    await createUserOrderStatusNotification(
+      orderId,
+      order.orderNumber,
+      order.userId,
+      updateStatus
     );
   }
 
