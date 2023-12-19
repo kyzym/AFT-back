c7612084d1811425e898f53d1c8cb41473ff6fbb
NikolayVerstak <verstak.n.a@gmail.com>
2023-12-19 11:07:43 +0200

    bugfix: Add cors options for logout clear cookies

--- controllers/users/auth/logout/index.js
+++ controllers/users/auth/logout/index.js
@@ -1,11 +1,21 @@
-import { tokenCookie, tokenType } from '#constants/index.js';
+import { tokenType } from '#constants/index.js';
 import { ctrlWrapper } from '#middlewares/ctrlWrapper.js';
 
 const controller = async (req, res) => {
   const { refreshToken, accessToken } = req.cookies;
 
-  accessToken && res.clearCookie(tokenCookie[tokenType.ACCESS].name);
-  refreshToken && res.clearCookie(tokenCookie[tokenType.REFRESH].name);
+  accessToken &&
+    res.clearCookie(tokenType.ACCESS, {
+      httpOnly: true,
+      sameSite: 'none',
+      secure: true,
+    });
+  refreshToken &&
+    res.clearCookie(tokenType.REFRESH, {
+      httpOnly: true,
+      sameSite: 'none',
+      secure: true,
+    });
 
   return res.status(200).json({
     success: true,
